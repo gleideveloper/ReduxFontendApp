cd1a55063515c9909e26bfff127acd3395ff8cff
Gleides Vinente <gleidevelop@gmail.com>
2023-07-25 01:31:00 -0400

    Comment about whitelist

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -6,7 +6,7 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['todos'],
+    whitelist: ['todos'],// Aqui você especifica quais reducers devem ser persistidos (neste caso, apenas o 'todos')
 };
 
 const rootReducer = combineReducers({
